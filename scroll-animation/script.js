84d2cb8e40489805596a897027c4be70888a0f81
Ahnsj828 <ala828@naver.com>
2025-07-26 04:08:34 +0900

    scroll-animation

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,5 +1,14 @@
 const boxes = document.querySelectorAll(".box");
 
+// 왼쪽/오른쪽 번갈아가며 클래스 지정
+boxes.forEach((box, idx) => {
+  if (idx % 2 === 0) {
+    box.classList.add("left"); // 짝수 index → 왼쪽
+  } else {
+    box.classList.add("right"); // 홀수 index → 오른쪽
+  }
+});
+
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
 
@@ -14,8 +23,5 @@ function checkBoxes() {
   });
 }
 
-// 스크롤 이벤트에 함수 연결
 window.addEventListener("scroll", checkBoxes);
-
-// 페이지 처음 로드 시에도 체크
 checkBoxes();
